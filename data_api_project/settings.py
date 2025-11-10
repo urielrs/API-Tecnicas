7518b34561c21b4c92c6ff6a20df93224272985d
Uriel Rodríguez <162546336+urielrs@users.noreply.github.com>
2025-11-10 02:45:49 -0600

    Update settings.py

--- data_api_project/settings.py
+++ data_api_project/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Si estás usando Whitenoise (lo cual es recomendado para Render)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
